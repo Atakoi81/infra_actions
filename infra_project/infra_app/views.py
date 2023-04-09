83f2e219b6f9c5fb4f16d0547060ea753aa28405
Сергей Твердохлебов <Atakoi1981@yandex.ru>
2023-04-09 18:11:55 +0700

    Проверка deploy на боевом сервере (Яндекс.Облако)_12(мелкие изменения в проекте)

--- infra_project/infra_app/views.py
+++ infra_project/infra_app/views.py
@@ -2,7 +2,7 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse('У меня получилось!')
+    return HttpResponse('У меня пока не все получилось!, но получится!!!')
 
 
 def second_page(request):
